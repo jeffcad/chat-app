123ca686fc0da2282f726c2e1a79db1652e9c582
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-12-17 22:10:19 +0900

    Event acknowledgements, disable buttons

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,24 +1,52 @@
 const socket = io()
 
-socket.on('message', message => {
+// Elements
+const $messageForm = document.getElementById('message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.getElementById('send-location')
+
+socket.on('message', (message) => {
     console.log(message)
 })
 
-document.getElementById('message-form')
-    .addEventListener('submit', e => {
-        e.preventDefault()
-        const message = e.target.elements.message.value
-        socket.emit('sendMessage', message)
-    })
+$messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-document.getElementById('send-location')
-    .addEventListener('click', () => {
-        if (!navigator.geolocation) {
-            return alert('Geolocation is not supported by your browser.')
+    // disable form when message sent
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    const message = e.target.elements.message.value
+    socket.emit('sendMessage', message, (error) => {
+
+        // re-enable form
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
         }
 
-        navigator.geolocation.getCurrentPosition(position => {
-            const { latitude, longitude } = position.coords
-            socket.emit('sendLocation', { latitude, longitude })
+        console.log('Message delivered!')
+    })
+})
+
+$sendLocationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    // disable send location button while waiting
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords
+        socket.emit('sendLocation', { latitude, longitude }, () => {
+            console.log('Location shared!')
+
+            //re-enable button
+            $sendLocationButton.removeAttribute('disabled')
         })
     })
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+const Filter = require('bad-words')
 
 app.use(express.static('public'))
 
@@ -15,13 +16,19 @@ io.on('connection', socket => {
 
     socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on('sendMessage', message => {
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed.')
+        }
         io.emit('message', message)
+        callback()
     })
 
-    socket.on('sendLocation', location => {
+    socket.on('sendLocation', (location, callback) => {
         const { latitude, longitude } = location
         io.emit('message', `https://google.com/maps?q=${latitude},${longitude}`)
+        callback()
     })
 
     socket.on('disconnect', () => {
