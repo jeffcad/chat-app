26cb1c1650bb38a1a3af021fb7986973c2723b10
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-12-17 12:29:15 +0900

    Socket.io, basic message input & relay

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const express = require('express')
-
 const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
 app.use(express.static('public'))
 
+io.on('connection', socket => {
+    console.log('New WebSocket connection!')
+
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', message => {
+        io.emit('message', message)
+    })
+})
+
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Server running on port ${port}!`))
+server.listen(port, () => console.log(`Server running on port ${port}!`))
